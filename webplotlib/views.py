d625b0227f80a1f200a58c47370cdba5dc950692
Kai <k@limist.com>
2011-05-24 20:10:33 -0400

    Updated with new module name import, updated comments.

--- webplotlib/views.py
+++ webplotlib/views.py
@@ -2,7 +2,7 @@
 
 from django.http import HttpResponse, HttpResponseRedirect
 
-from webplotlib.chart_templates import create_chart_as_png_str
+from webplotlib.chart_builders import create_chart_as_png_str
 
 
 def index(request):
@@ -13,12 +13,14 @@ def show_ts_plot_png(request):
     fake_data_dct = {
         'data': [[1, 2, 1, 2, 3, -1, 4, -2, 2.5, 1.3]]}
     img_str = create_chart_as_png_str('timeseries', fake_data_dct, {}, '')
+    #
     # From here the Django-specific part follows; not much to it:
+    #
     response = HttpResponse(img_str, mimetype='image/png')
     # Development note: the function create_timeseries_png_str uses a
     # StringIO approach, which is better than some examples (on the
-    # web) using print_png(response) tried first (below) which causes
-    # timeout error messages on the server-side.
+    # web) using print_png(response); when I tried those (as below) it
+    # causes timeout error messages on the server-side.
     #
     # figure.print_png(response)
     return response
@@ -28,6 +30,5 @@ def show_bar_plot_png(request):
     fake_data_dct = {
         'data': [[1, 2, 1, 2, 3, -11, 4, -2, 2.5, 1.3]]}
     img_str = create_chart_as_png_str('barchart', fake_data_dct, {}, '')
-    # From here the Django-specific part follows; not much to it:
     response = HttpResponse(img_str, mimetype='image/png')
     return response
