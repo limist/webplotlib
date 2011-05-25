97810abf67132d9650f0a49a4fac47adf2ee3dd6
Kai <k@limist.com>
2011-05-24 20:03:23 -0400

    Updated with shorter name, numpy dependency, license, description, etc.

--- setup.py
+++ setup.py
@@ -1,29 +1,32 @@
 from setuptools import setup
 
 setup(
-    name='django-webplotlib',
+    name='webplotlib',
     packages=['webplotlib', ],
     version='0.1',
-    description='A Django app for serving charts/graphs using matplotlib.',
+    description='A package for creating server-side charts/graphs using matplotlib; examples are also given for serving those charts directly via Django.',
     author='Kai',
     author_email='k@limist.com',
-    url='http://github.com/limist/django-webplotlib',
+    url='http://github.com/limist/webplotlib',
     download_url='',
-    keywords="django chart graph matplotlib app",
-    license='BSD',
+    keywords="django chart graph matplotlib server-side",
+    license='GPL',
     install_requires=[
         'Django',
         'matplotlib',
+        'numpy',
         'pytest >= 2',
         'setuptools',
         ],
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Topic :: Multimedia :: Graphics',
         'Topic :: Internet :: WWW/HTTP'],
     long_description=open('README', 'r').read()
     )
